6e7dea26b9c9015d8478306dceb4003f35502e2a
envrs <230240030+envrs@users.noreply.github.com>
2026-09-07 12:30:49 +0000

    perf: reduce request ID generation overhead
    - Use random prefix + atomic counter instead of crypto/rand per request
    - Generates 16-char hex IDs with zero allocations after init
    - Still accepts X-Request-ID header when present

--- kit/middleware/middleware.go
+++ kit/middleware/middleware.go
@@ -4,13 +4,25 @@ import (
 	"context"
 	"net/http"
 	"crypto/rand"
+	"encoding/binary"
 	"encoding/hex"
+	"sync/atomic"
 )
 
 type (
 	RequestKey         struct{}
 	ResponseHeadersKey struct{}
 )
+
+var requestPrefix [8]byte
+
+func init() {
+	if _, err := rand.Read(requestPrefix[:]); err != nil {
+		requestPrefix = [8]byte{'s', 'u', 'p', 'e', 'r', 'k', 'i', 't'}
+	}
+}
+
+var requestCounter atomic.Uint64
 
 func WithRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -32,10 +44,8 @@ func WithRequestID(next http.Handler) http.Handler {
 }
 
 func generateRequestID() string {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		return "unknown"
-	}
-	return hex.EncodeToString(b)
+	var b [16]byte
+	copy(b[:8], requestPrefix[:])
+	binary.BigEndian.PutUint64(b[8:], requestCounter.Add(1))
+	return hex.EncodeToString(b[:])
 }
